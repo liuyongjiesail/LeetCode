e66dd77fe85c54c7c3630eb785978a16f049adb0
liuyongjie <liuyongjiesail@icloud.com>
2019-02-14 20:37:53 +0800

    [1] Two Sum

---
+++ 1.two-sum.c
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=1 lang=c
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (40.25%)
+ * Total Accepted:    1.4M
+ * Total Submissions: 3.5M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target) {
+    static int result[2];
+    for(int i = 0; i < numsSize; i++) {
+        for(int j = i + 1; j < numsSize; j++) {
+            if(nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                break;
+            }
+        }
+    }
+    return result;
+}
