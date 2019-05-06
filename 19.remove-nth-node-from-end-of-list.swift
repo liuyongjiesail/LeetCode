974985e2923b91f9b60a0b9a7fccedb7fab2b1c3
liuyongjie <liuyongjiesail@icloud.com>
2019-05-06 21:10:30 +0800

    [19] Remove Nth Node From End of List [Two pass algorithm]

---
+++ 19.remove-nth-node-from-end-of-list.swift
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=19 lang=swift
+ *
+ * [19] Remove Nth Node From End of List
+ *
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (34.10%)
+ * Total Accepted:    383.7K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given a linked list, remove the n-th node from the end of list and return
+ * its head.
+ * 
+ * Example:
+ * 
+ * 
+ * Given linked list: 1->2->3->4->5, and n = 2.
+ * 
+ * After removing the second node from the end, the linked list becomes
+ * 1->2->3->5.
+ * 
+ * 
+ * Note:
+ * 
+ * Given n will always be valid.
+ * 
+ * Follow up:
+ * 
+ * Could you do this in one pass?
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        
+        let dummy = ListNode(0)
+        dummy.next = head
+        
+        var length = 0
+        var first = head
+        
+        while (first != nil) {
+            length = length + 1
+            first = first?.next
+        }
+        
+        length = length - n
+        first = dummy
+        while length > 0 {
+            length = length - 1
+            first = first?.next
+        }
+        
+        first?.next = first?.next?.next
+        
+        return dummy.next
+    }
+    
+}
+
